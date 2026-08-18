94ef0a9e67412a60af37419ddc4eb09b37ed16af
Sainzel <cppnaf@gmail.com>
2026-08-18 15:07:17 +0700

    menambah catatan

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './App.css'
 import App from './App.jsx'
-import Test from './Test.jsx'
 // import Condition from './Condition.jsx'
 // import Map from './Map.jsx'
 // import Event from './Event.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <App /> */}
-    <Test />
+    <App />
+    {/* <Test /> */}
     {/* <Map/>
     <Condition/>
     <Event/> */}
