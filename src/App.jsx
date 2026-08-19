102255d8d592af5ed281400a0e94fd006447e517
Sainzel <cppnaf@gmail.com>
2026-08-19 10:29:08 +0700

    pecah components

--- src/App.jsx
+++ src/App.jsx
@@ -41,7 +41,54 @@ const animesData = [
 ];
 
 export default function App() {
+
+  return (
+    <>
+    <NavBar />
+    <Main />
+    </>
+  );
+}
+
+function NavBar () {
+  return (
+    <nav className="nav-bar">
+        <Logo />
+        <NavBar />
+    </nav>
+  )
+}
+
+function Logo () {
+  return (
+    <div className="logo">
+          <span role="img">🍥</span>
+          <h1>WeeBoo</h1>
+          <span role="img">🍥</span>
+    </div>
+  )
+}
+
+function Search () {
   const [query, setQuery] = useState('');
+
+  return (
+    <div className="search-container">
+          <input className="search" type="text" placeholder="Search anime..." value={query} onChange={(e) => setQuery(e.target.value)} />
+          <NumResult />
+    </div>
+  )
+}
+
+function NumResult () {
+  return (
+     <p className="search-results">
+        Found <strong>4</strong> results
+    </p>
+  )
+}
+
+function Main () {
   const [animes, setAnimes] = useState(animesData);
   const [selectedAnime, setSelectedAnime] = useState(animes[0]);
   const [isOpen1, setIsOpen1] = useState(true);
@@ -53,22 +100,7 @@ export default function App() {
   }
 
   return (
-    <>
-      <nav className="nav-bar">
-        <div className="logo">
-          <span role="img">🍥</span>
-          <h1>WeeBoo</h1>
-          <span role="img">🍥</span>
-        </div>
-        <div className="search-container">
-          <input className="search" type="text" placeholder="Search anime..." value={query} onChange={(e) => setQuery(e.target.value)} />
-          <p className="search-results">
-            Found <strong>4</strong> results
-          </p>
-        </div>
-      </nav>
-
-      <main className="main">
+          <main className="main">
         <div className="box">
          <button className="btn-toggle" onClick={() => setIsOpen1((open) => !open)}>  {/* akan membalikan tombol, kalau bernilai true -, jika di click akan menjalanka kebalikan nya false + */}
             {isOpen1 ? '–' : '+'}
@@ -114,6 +146,5 @@ export default function App() {
           )}
         </div>
       </main>
-    </>
-  );
+  )
 }
